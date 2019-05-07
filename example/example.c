dde2f1034a3db578990a2af5ec515faac9786e5b
langliverygood <951328469@qq.com>
2019-05-07 20:15:10 +0800

    修改示例文件

--- example/example.c
+++ example/example.c
@@ -2,30 +2,41 @@
 
 #include "lshell.h"
 
-void test_cd(int argc, char **argv)
+void ex_show(int argc, char **argv)
 {
 	int i;
 	
-	printf("cd\n");
-	printf("%d\n", argc);
+	printf("This func is : ex_show\n");
+	printf("argc: %d\n", argc);
+	printf("argv:");
 	for(i = 0; i < argc; i++)
 	{
-		printf("%s\n", argv[i]);
+		printf("%s ", argv[i]);
 	}
+	printf("\n");
 	
 	return;
 }
 
-void test_ls(int argc, char **argv)
+void ex_show_argc(int argc, char **argv)
+{
+	printf("This func is : ex_show_argc\n");
+	printf("argc: %d\n", argc);
+	
+	return;
+}
+
+void ex_show_argv(int argc, char **argv)
 {
 	int i;
 	
-	printf("ls\n");
-	printf("%d\n", argc);
+	printf("This func is : ex_show_argv\n");
+	printf("argv:");
 	for(i = 0; i < argc; i++)
 	{
-		printf("%s\n", argv[i]);
+		printf("%s ", argv[i]);
 	}
+	printf("\n");
 	
 	return;
 }
@@ -37,8 +48,9 @@ int main(int argc, char **argv)
 	lshell_init();
 	lshell_set_promt("ex");
 	lshell_set_errmsg_swtich(1);
-	ret = lshell_register(-1, "ls", "ls", test_ls, RUN_AT_MAIN_THREAD, 0, 0, 0);
-	lshell_register(ret, "cd", "cd", test_cd, RUN_AT_MAIN_THREAD, 0, 0, 0);
+	ret = lshell_register(-1, "show", "show argc and argv", ex_show, RUN_AT_MAIN_THREAD, 0, 0, 0);
+	lshell_register(ret, "argc", "show argc", ex_show_argc, RUN_AT_MAIN_THREAD, 0, 0, 0);
+	lshell_register(ret, "argv", "show argv", ex_show_argv, RUN_AT_MAIN_THREAD, 0, 0, 0);
 	lshell_start();
 		
 	return 0;		
